1ea063e585409a15b658887f5d3993ab915b4c14
rahul <rahuljain5355@gmail.com>
2021-06-10 14:13:02 +0530

    First Commit

---
+++ src/seleniumTest/FirstSeleniumProgram.java
@@ -0,0 +1,17 @@
+package seleniumTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstSeleniumProgram {
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Rahul\\Desktop\\Selenium\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		
+	}
+
+}
